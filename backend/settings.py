aeb954aec1f248d8103b5187136cd1a317ae3f57
Amal-lal <amallal2004@gmail.com>
2025-07-16 00:15:25 +0530

    Set up PostgreSQL with Supabase, decouple, dj-database-url

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,6 +90,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Level, Submission, Profile, Session, Attendance
 
-# Register your models here.
+admin.site.register(Level)
+admin.site.register(Submission)
+admin.site.register(Profile)
+admin.site.register(Session)
+admin.site.register(Attendance)
